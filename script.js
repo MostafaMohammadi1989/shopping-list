6f42b0630f9263d195ce39118788559d5a478270
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-21 00:18:50 +0330

    write function for adding data in input in the ul list

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-var button = document.getElementsByTagName("button")[0];
+var input = document.getElementById("input");
+var button = document.getElementById("enter");
+var ul = document.querySelector("ul");
 
 button.addEventListener("click", function () {
-  console.log("The click Button is working");
+  if (input.value.length > 0) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+  }
+});
+
+input.addEventListener("keypress", function (event) {
+  if (input.value.length > 0 && event.which === 13) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+  }
 });
