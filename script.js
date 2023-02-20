5256ec3b4a60831a4a48d7b8cc28d146eb439939
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-21 00:30:24 +0330

    reusable function in script file

--- script.js
+++ script.js
@@ -2,20 +2,33 @@ var input = document.getElementById("input");
 var button = document.getElementById("enter");
 var ul = document.querySelector("ul");
 
+function createLi() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+
+function inputLength() {
+  return input.value.length;
+}
+
 button.addEventListener("click", function () {
-  if (input.value.length > 0) {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
+  if (inputLength() > 0) {
+    // var li = document.createElement("li");
+    // li.appendChild(document.createTextNode(input.value));
+    // ul.appendChild(li);
+    // input.value = "";
+    createLi();
   }
 });
 
 input.addEventListener("keypress", function (event) {
-  if (input.value.length > 0 && event.which === 13) {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
+  if (inputLength() > 0 && event.which === 13) {
+    // var li = document.createElement("li");
+    // li.appendChild(document.createTextNode(input.value));
+    // ul.appendChild(li);
+    // input.value = "";
+    createLi();
   }
 });
